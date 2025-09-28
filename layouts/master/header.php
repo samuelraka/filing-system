d64c463b1e224a15816a3617a6ee150402b194d1
samuelraka <139964724+samuelraka@users.noreply.github.com>
2025-09-28 23:41:27 +0700

    dummy data chart

--- layouts/master/header.php
+++ layouts/master/header.php
@@ -6,5 +6,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../assets/css/tailwind.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title></title>
 </head>
